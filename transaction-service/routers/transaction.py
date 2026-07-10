ee33ec2fa0d70867e52c7b5cf4197b6d336870c9
nozilis <kreonix06110611@gmail.com>
2026-07-10 17:50:14 +0300

    feat(transaction-service): add get_transactions endpoint with pagination

--- transaction-service/routers/transaction.py
+++ transaction-service/routers/transaction.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, HTTPException
 from dependencies import get_current_user, get_db
 from sqlalchemy.ext.asyncio import AsyncSession
 from models import Transaction
 from schemas import TransactionCreate, TransactionResponse
+from sqlalchemy import select
 
 router = APIRouter(
     prefix='/transactions',
@@ -15,3 +16,9 @@ async def create_transaction(transaction: TransactionCreate, user_id: int = Depe
     db.add(to_db_transaction)
     await db.commit()
     return TransactionResponse.model_validate(to_db_transaction)
+
+@router.get('/', status_code=status.HTTP_200_OK)
+async def get_transactions(user_id: int = Depends(get_current_user), db: AsyncSession = Depends(get_db), page: int = 1, page_size: int = 20):
+    result = await db.execute(select(Transaction).where(Transaction.user_id == user_id).limit(page_size).offset((page-1)*page_size))
+    db_transactions = result.scalars().all()
+    return [TransactionResponse.model_validate(t) for t in db_transactions]
