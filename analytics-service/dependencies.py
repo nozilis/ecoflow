85011675210dcd4b8d62787d8b68fd816ea9db67
nozilis <kreonix06110611@gmail.com>
2026-07-14 15:15:52 +0300

    feat(analytics-service): add get_current_user func for verify user

--- analytics-service/dependencies.py
+++ analytics-service/dependencies.py
@@ -1,7 +1,31 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import AsyncGenerator
 from database import async_session_maker
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+from decouple import config
+from fastapi import HTTPException, status, Depends
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+ALGORITHM = "HS256"
+
+def decode_token(token: str):
+    try:
+        payload = jwt.decode(token, config('SECRET_KEY'), algorithms=[ALGORITHM])
+        return payload
+    except JWTError:    
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    payload = decode_token(token)
+    user_id = int(payload.get("sub"))
+    return user_id
