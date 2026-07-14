ea87783d88c079e6f5dae327af5dec0f8847e3dc
nozilis <kreonix06110611@gmail.com>
2026-07-14 16:08:52 +0300

    feat(analytics-service): add get_monthly_stats endpoint

---
+++ analytics-service/routers/analytics.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, status, Depends
+from dependencies import get_db, get_current_user
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from models import MonthlyStats
+from schemas import MonthlyStatsResponse
+from datetime import datetime
+
+router = APIRouter(
+    prefix='/analytics',
+    tags=['analytics']
+)
+
+@router.get('/monthly_stats', status_code=status.HTTP_200_OK)
+async def get_monthly_stats(month: int = None, year: int = None, db: AsyncSession = Depends(get_db), user: int = Depends(get_current_user)):
+    now = datetime.now()
+    if month is None:
+       month = now.month
+    if year is None:
+        year = now.year
+    result = await db.execute(select(MonthlyStats).where(MonthlyStats.user_id == user, MonthlyStats.month == month, MonthlyStats.year == year))
+    db_stats = result.scalars().all()
+    return [MonthlyStatsResponse.model_validate(s) for s in db_stats]
