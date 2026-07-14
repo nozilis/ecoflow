dc8b52bb5856ca39de2520ea1e1c8100d0a703e9
nozilis <kreonix06110611@gmail.com>
2026-07-14 13:22:38 +0300

    feat(analytics-service): add MonthlyStats model

---
+++ analytics-service/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, Integer, Date, func, UniqueConstraint
+from sqlalchemy.ext.hybrid import hybrid_property
+from datetime import date
+
+class Base(DeclarativeBase):
+    pass
+
+class MonthlyStats(Base):
+    __tablename__ = 'MonthlyStats'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer)
+    year: Mapped[int] = mapped_column(Integer)
+    month: Mapped[int] = mapped_column(Integer)
+    category: Mapped[str] = mapped_column(String(25))
+    transaction_type: Mapped[str] = mapped_column(String(25))
+    total_amount: Mapped[int] = mapped_column(Integer)
+    
+    __table_args__ = (UniqueConstraint('user_id', 'year', 'month', 'category', name='month_category_agregation'))
