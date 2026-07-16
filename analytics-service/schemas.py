7195fc327435a4c5643a165bf9a2da1099d20af8
nozilis <kreonix06110611@gmail.com>
2026-07-16 11:02:47 +0300

    feat(analytics-service): add YearlyStatsResponse schema

--- analytics-service/schemas.py
+++ analytics-service/schemas.py
@@ -8,3 +8,10 @@ class MonthlyStatsResponse(BaseModel):
     category: str
     transaction_type: str
     total_amount: int
+
+class YearlyStatsResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    year: int
+    category: str
+    total_year_amount: int
