e01d7a12fbed623fc1b127edec0454ac7bd0811a
nozilis <kreonix06110611@gmail.com>
2026-07-18 20:46:03 +0300

    feat(transaction-service): add created_at field and serializing it for message body

--- transaction-service/publisher.py
+++ transaction-service/publisher.py
@@ -1,6 +1,7 @@
 import aio_pika
 from decouple import config
 import json
+from datetime import datetime
 
 async def publish_event(message_body, routing_key):
     connection = await aio_pika.connect_robust(
@@ -24,7 +25,7 @@ async def publish_event(message_body, routing_key):
 
         print(f" Sent: {message_body}")
         
-async def publish_transaction_created(user_id, amount, transaction_type, category):
-    data_dict = {'user_id': user_id, 'amount': amount, 'transaction_type': transaction_type.value, 'category': category.value} 
-    message_body = json.dumps(data_dict).encode("utf-8")
+async def publish_transaction_created(user_id, amount, transaction_type, category, created_at):
+    data_dict = {'user_id': user_id, 'amount': amount, 'transaction_type': transaction_type.value, 'category': category.value, 'created_at': created_at} 
+    message_body = json.dumps(data_dict, default=lambda o: o.isoformat() if isinstance(o, datetime) else None).encode("utf-8")
     await publish_event(message_body, 'transaction.created')
