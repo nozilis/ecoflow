71b8c633e33d67e5fd66f67747c3a2160912045b
nozilis <kreonix06110611@gmail.com>
2026-07-19 18:52:16 +0300

    feat(notification-service): add NotificationLog model

--- notification-service/models.py
+++ notification-service/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy import String, Boolean, ForeignKey, DateTime, func
+from datetime import datetime
 
 class Base(DeclarativeBase):
     pass
@@ -17,3 +18,12 @@ class NotificationSettings(Base):
     user_id: Mapped[int] = mapped_column(ForeignKey('user_contact.user_id', ondelete='CASCADE'), primary_key=True)
     monthly_budget_exceeded_notification: Mapped[bool] = mapped_column(Boolean, default=True)
     weekly_summary_notification: Mapped[bool] = mapped_column(Boolean, default=True)
+
+class NotificationLog(Base):
+    __tablename__ = 'notifications_history'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user_contact.user_id'))
+    notification_topic: Mapped[str] = mapped_column(String(200))
+    notification_message: Mapped[str] = mapped_column(String(2000))
+    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
