2f5c4d08ddb330ded0dfe82d8684aecb14e97f5c
nozilis <kreonix06110611@gmail.com>
2026-07-19 18:51:35 +0300

    feat(notification-service): add NotificationSettings model

--- notification-service/models.py
+++ notification-service/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import String
+from sqlalchemy import String, Boolean, ForeignKey
 
 class Base(DeclarativeBase):
     pass
@@ -10,3 +10,10 @@ class UserContact(Base):
     user_id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
     username: Mapped[str] = mapped_column(String(50), unique=True)
+
+class NotificationSettings(Base):
+    __tablename__ = 'notifications_settings'
+
+    user_id: Mapped[int] = mapped_column(ForeignKey('user_contact.user_id', ondelete='CASCADE'), primary_key=True)
+    monthly_budget_exceeded_notification: Mapped[bool] = mapped_column(Boolean, default=True)
+    weekly_summary_notification: Mapped[bool] = mapped_column(Boolean, default=True)
