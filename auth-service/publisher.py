6c0b317b5d37df6789456499ef24d183435e8c29
nozilis <kreonix06110611@gmail.com>
2026-07-21 03:40:18 +0300

    feat(auth-service): configure publish_event for user.created event

---
+++ auth-service/publisher.py
@@ -0,0 +1,30 @@
+import aio_pika
+from decouple import config
+import json
+
+async def publish_event(message_body, routing_key):
+    connection = await aio_pika.connect_robust(
+        f"amqp://{config('RABBITMQ_DEFAULT_USER')}:{config('RABBITMQ_DEFAULT_PASS')}@rabbitmq/"
+    )
+
+    async with connection:
+        channel = await connection.channel()
+
+        message = aio_pika.Message(
+            body=message_body,
+            delivery_mode=aio_pika.DeliveryMode.PERSISTENT
+        )
+
+        exchange = await channel.declare_exchange("ecoflow_events", aio_pika.ExchangeType.TOPIC, durable=True)
+
+        await exchange.publish(
+            message=message,
+            routing_key=routing_key
+        )
+
+        print(f" Sent: {message_body}")
+        
+async def publish_user_created(user_id, username, email):
+    data_dict = {'id': user_id, 'username': username, 'email': email}
+    message_body = json.dumps(data_dict).encode("utf-8")
+    await publish_event(message_body, 'user.created')
