9ac1ff9799cc6e793bdddba4999fc2d7b5e7630d
nozilis <kreonix06110611@gmail.com>
2026-07-21 16:07:23 +0300

    feat(user-service): add UserProfile model

---
+++ user-service/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, JSON, Enum
+from enums import VisibilityChoice
+from typing import Dict
+
+class Base(DeclarativeBase):
+    pass
+
+class UserProfile(Base):
+    __tablename__ = 'user_profiles'
+
+    user_id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(50), unique=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    avatar: Mapped[str] = mapped_column(String(300), nullable=True)
+    biography: Mapped[str] = mapped_column(String(750), nullable=True)
+    social_links: Mapped[Dict[str, str]] = mapped_column(JSON, default=dict)
+    visibility_choice: Mapped[VisibilityChoice] = mapped_column(Enum(VisibilityChoice), default=VisibilityChoice.PRIVATE)
