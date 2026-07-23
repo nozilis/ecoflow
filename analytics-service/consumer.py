3a962684ecc2d673d0bbd6d4bfefd6121777a735
nozilis <kreonix06110611@gmail.com>
2026-07-23 14:17:59 +0300

    refactor(analytics-service): rework consumer to use generic run_consumer pattern

--- analytics-service/consumer.py
+++ analytics-service/consumer.py
@@ -6,8 +6,11 @@ from database import async_session_maker
 from datetime import datetime
 from sqlalchemy import select
 from models import MonthlyStats
+import logging
 
-async def consume_event(routing_key):
+logger = logging.getLogger(__name__)
+
+async def run_consumer(handlers: dict[str, callable]):
     connection = await aio_pika.connect_robust(
         f"amqp://{config('RABBITMQ_DEFAULT_USER')}:{config('RABBITMQ_DEFAULT_PASS')}@rabbitmq/"
     )
@@ -17,16 +20,19 @@ async def consume_event(routing_key):
 
         exchange = await channel.declare_exchange("ecoflow_events", aio_pika.ExchangeType.TOPIC, durable=True)
 
-        queue = await channel.declare_queue("analytics_transaction_created", durable=True)
+        queue = await channel.declare_queue("analytics_service.events", durable=True)
 
-        await queue.bind(exchange, routing_key=routing_key)
+        for routing_key in handlers.keys():
+            await queue.bind(exchange, routing_key=routing_key)
         
         async with queue.iterator() as queue_iter:
             async for message in queue_iter:
                 async with message.process():
                     data = json.loads(message.body)
-                    async with async_session_maker() as session: 
-                        await handle_transaction_created(data, session)
+                    handler = handlers.get(message.routing_key)
+                    if handler:
+                        async with async_session_maker() as session: 
+                            await handler(data, session)
 
 async def handle_transaction_created(data: dict, session):
     date = datetime.fromisoformat(data['created_at'])
@@ -36,9 +42,15 @@ async def handle_transaction_created(data: dict, session):
     if db_monthly_stats is None:
         db_monthly_stats = MonthlyStats(user_id = data['user_id'], year = year, month = month, category = data['category'], transaction_type = data['transaction_type'], total_amount = data['amount'])
         session.add(db_monthly_stats)
+        logger.info('MonthlyStats object successfully created')
     else:
         db_monthly_stats.total_amount += data['amount']
+        logger.info('Total amount successfully increased')
     await session.commit()
 
 if __name__ == "__main__":
-    asyncio.run(consume_event(('transaction.created')))
+    tasks = [
+        run_consumer({'transaction.created': handle_transaction_created})
+    ]
+
+    asyncio.run(asyncio.gather(*tasks))
