296e393308ae219214d21573d675200477b2359e
nozilis <kreonix06110611@gmail.com>
2026-07-24 23:09:42 +0300

    refactor(transaction-service): rework endpoints for project consistency

--- transaction-service/routers/transaction.py
+++ transaction-service/routers/transaction.py
@@ -23,15 +23,15 @@ async def create_transaction(transaction: TransactionCreate, user_id: int = Depe
 
 @router.get('/', status_code=status.HTTP_200_OK)
 async def get_transactions(user_id: int = Depends(get_current_user), db: AsyncSession = Depends(get_db), page: int = 1, page_size: int = 20):
-    result = await db.execute(select(Transaction).where(Transaction.user_id == user_id).limit(page_size).offset((page-1)*page_size))
-    db_transactions = result.scalars().all()
+    transactions = await db.execute(select(Transaction).where(Transaction.user_id == user_id).limit(page_size).offset((page-1)*page_size))
+    db_transactions = transactions.scalars().all()
     return [TransactionResponse.model_validate(t) for t in db_transactions]
 
 @router.delete('/{transaction_id}', status_code=status.HTTP_204_NO_CONTENT)
 async def delete_transaction(transaction_id: int, user_id: int = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
-    db_transaction_is_exist = await db.execute(select(Transaction).where(Transaction.id == transaction_id, Transaction.user_id == user_id)) 
-    db_transaction = db_transaction_is_exist.scalar_one_or_none()
-    if not db_transaction:
+    transaction_is_exist = await db.execute(select(Transaction).where(Transaction.id == transaction_id, Transaction.user_id == user_id)) 
+    db_transaction = transaction_is_exist.scalar_one_or_none()
+    if db_transaction is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Транзакция не найдена')
     await db.delete(db_transaction)
     await db.commit()
@@ -39,9 +39,9 @@ async def delete_transaction(transaction_id: int, user_id: int = Depends(get_cur
 
 @router.patch('/{transaction_id}', status_code=status.HTTP_200_OK)
 async def update_transaction(transaction_id: int, transaction_update_request: TransactionUpdate, user_id: int = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
-    db_transaction_is_exist = await db.execute(select(Transaction).where(Transaction.user_id == user_id, Transaction.id == transaction_id))
-    db_transaction = db_transaction_is_exist.scalar_one_or_none()
-    if not db_transaction:
+    transaction_is_exist = await db.execute(select(Transaction).where(Transaction.user_id == user_id, Transaction.id == transaction_id))
+    db_transaction = transaction_is_exist.scalar_one_or_none()
+    if db_transaction is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Транзакция не найдена')
     recent_amount, recent_type = db_transaction.amount, db_transaction.transaction_type
     transaction_update_dump = transaction_update_request.model_dump(exclude_unset=True)
