3838d223b786812a87bea7f9a3354220bbb7a58f
nozilis <kreonix06110611@gmail.com>
2026-07-25 19:44:43 +0300

    feat(auth-service): add user.updated handler

---
+++ auth-service/consumer.py
@@ -0,0 +1,55 @@
+import asyncio
+import aio_pika
+from decouple import config
+import json
+from database import async_session_maker
+import logging
+from sqlalchemy import select
+from models import User
+from sqlalchemy.ext.asyncio import AsyncSession
+
+logger = logging.getLogger(__name__)
+
+async def run_consumer(handlers: dict[str, callable]):
+    connection = await aio_pika.connect_robust(
+        f"amqp://{config('RABBITMQ_DEFAULT_USER')}:{config('RABBITMQ_DEFAULT_PASS')}@rabbitmq/"
+    )
+
+    async with connection:
+        channel = await connection.channel()
+
+        exchange = await channel.declare_exchange("ecoflow_events", aio_pika.ExchangeType.TOPIC, durable=True)
+
+        queue = await channel.declare_queue('user_service.events', durable=True)
+
+        for routing_key in handlers.keys():
+            await queue.bind(exchange, routing_key=routing_key)
+        
+        async with queue.iterator() as queue_iter:
+            async for message in queue_iter:
+                async with message.process():
+                    data = json.loads(message.body)
+                    handler = handlers.get(message.routing_key)
+                    if handler:
+                        async with async_session_maker() as session: 
+                            await handler(data, session)
+
+async def handle_user_updated(data: dict, session: AsyncSession):
+    user_id, username, email = data['user_id'], data.get('username'), data.get('email')
+    user_is_exist = await session.execute(select(User).where(User.id == user_id))
+    db_user = user_is_exist.scalar_one_or_none()
+    if db_user is None:
+        logger.info('User not found')
+    else:
+        if username:
+            db_user.username = username
+        if email:
+            db_user.email = email
+        await session.commit()
+        logger.info('User successfully updated')
+
+if __name__ == '__main__':
+    handlers = {
+        'user.updated': handle_user_updated,
+    }
+    asyncio.run(run_consumer(handlers))
