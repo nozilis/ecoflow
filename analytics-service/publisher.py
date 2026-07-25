f835eb758a6ea21d10b76e5ad4479b69f60af069
nozilis <kreonix06110611@gmail.com>
2026-07-25 14:43:29 +0300

    feat(analytics-service): configure publisher for analytics service

---
+++ analytics-service/publisher.py
@@ -0,0 +1,30 @@
+import aio_pika
+from decouple import config
+import json
+
+async def publish_event(message_body, routing_key):
+    connection = await aio_pika.connect_robust(
+        f"amqp://{config('RABBITMQ_DEFAULT_USER')}:{config('RABBITMQ_DEFAULT_PASS')}@rabbitmq/"
+    )
+
+    async with connection:
+        channel = await connection.channel()
+
+        message = aio_pika.Message(
+            body=message_body,
+            delivery_mode=aio_pika.DeliveryMode.PERSISTENT
+        )
+
+        exchange = await channel.declare_exchange("ecoflow_events", aio_pika.ExchangeType.TOPIC, durable=True)
+
+        await exchange.publish(
+            message=message,
+            routing_key=routing_key
+        )
+
+        print(f" Sent: {message_body}")
+
+async def publish_analytics_events(event_type, user_id, **kwargs):
+    data_dict = {'user_id': user_id, **kwargs}
+    message_body = json.dumps(data_dict).encode("utf-8")
+    await publish_event(message_body, f'budget.{event_type}')
