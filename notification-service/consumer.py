192655593426ec13d63d9ed9ac4769884e5faaee
nozilis <kreonix06110611@gmail.com>
2026-07-25 19:47:54 +0300

    feat(notification-service): add user.deleted handler

--- notification-service/consumer.py
+++ notification-service/consumer.py
@@ -62,6 +62,16 @@ async def handle_user_updated(data: dict, session: AsyncSession):
         await session.commit()
         logger.info('User successfully updated')
 
+async def handle_user_deleted(data: dict, session: AsyncSession):
+    user_id = data['user_id']
+    user_contact_is_exist = await session.execute(select(UserContact).where(UserContact.user_id == user_id))
+    db_user_contact = user_contact_is_exist.scalar_one_or_none()
+    if db_user_contact is None:
+            logger.info('User not found')
+    else:
+        await session.delete(db_user_contact)
+        await session.commit()
+
 async def handle_budget_exceed(data: dict, session: AsyncSession):
     user_id, monthly_stats_total, user_budget_limit = data['user_id'], data['monthly_stats_total'], data['user_budget_limit']
     difference = monthly_stats_total - user_budget_limit
