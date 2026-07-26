906ea0123237662bdd238116089832cf10a88fd9
nozilis <kreonix06110611@gmail.com>
2026-07-26 21:52:05 +0300

    fix(notification-service): rename foreign key fields

--- notification-service/models.py
+++ notification-service/models.py
@@ -15,7 +15,7 @@ class UserContact(Base):
 class NotificationSettings(Base):
     __tablename__ = 'notifications_settings'
 
-    user_id: Mapped[int] = mapped_column(ForeignKey('user_contact.user_id', ondelete='CASCADE'), primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user_contacts.user_id', ondelete='CASCADE'), primary_key=True)
     monthly_budget_exceeded_notification: Mapped[bool] = mapped_column(Boolean, default=True)
     weekly_summary_notification: Mapped[bool] = mapped_column(Boolean, default=True)
 
@@ -23,7 +23,7 @@ class NotificationLog(Base):
     __tablename__ = 'notifications_logs'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey('user_contact.user_id'))
+    user_id: Mapped[int] = mapped_column(ForeignKey('user_contacts.user_id'))
     notification_topic: Mapped[str] = mapped_column(String(200))
     notification_message: Mapped[str] = mapped_column(String(2000))
     created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
