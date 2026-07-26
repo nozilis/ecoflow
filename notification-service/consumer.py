398e6a310d40dfd272c6e4fb6f73166e0a1acb9a
nozilis <kreonix06110611@gmail.com>
2026-07-26 16:32:10 +0300

    refactor(notification-service): add logging

--- notification-service/consumer.py
+++ notification-service/consumer.py
@@ -44,33 +44,34 @@ async def handle_user_created(data: dict, session: AsyncSession):
         session.add(create_user_contact)
         session.add(create_notification_settings)
         await session.commit()
-        logger.info(f"User {username} successfully created")
+        logger.info(f"User {user_id} successfully created")
     else:
-        logger.info(f"User {username} already exists in UserContact, skipping")
+        logger.warning(f"User {user_id} already exists in UserContact, skipping")
 
 async def handle_user_updated(data: dict, session: AsyncSession):
     user_id, username, email = data['user_id'], data.get('username'), data.get('email')
     user_contact_is_exist = await session.execute(select(UserContact).where(UserContact.user_id == user_id))
     db_user_contact = user_contact_is_exist.scalar_one_or_none()
     if db_user_contact is None:
-        logger.info('User not found')
+        logger.warning(f'User {user_id} not found')
     else:
         if username:
             db_user_contact.username = username
         if email:
             db_user_contact.email = email
         await session.commit()
-        logger.info('User successfully updated')
+        logger.info(f'User {user_id} successfully updated')
 
 async def handle_user_deleted(data: dict, session: AsyncSession):
     user_id = data['user_id']
     user_contact_is_exist = await session.execute(select(UserContact).where(UserContact.user_id == user_id))
     db_user_contact = user_contact_is_exist.scalar_one_or_none()
     if db_user_contact is None:
-            logger.info('User not found')
+            logger.warning(f'User {user_id} not found')
     else:
         await session.delete(db_user_contact)
         await session.commit()
+        logger.info(f'User {user_id} successfully deleted')
 
 async def handle_budget_exceed(data: dict, session: AsyncSession):
     user_id, monthly_stats_total, user_budget_limit = data['user_id'], data['monthly_stats_total'], data['user_budget_limit']
@@ -78,12 +79,12 @@ async def handle_budget_exceed(data: dict, session: AsyncSession):
     username = await session.execute(select(UserContact.username).where(UserContact.user_id == user_id))
     db_username = username.scalar_one_or_none()
     if db_username is None:
-        logger.info('User not found')
+        logger.warning(f'User {user_id} not found')
     else:
         notification_topic = 'Budget exceed'
         notification_message = f'Hello, {db_username}, your spending has exceeded the expected limit by {difference}'
-        print('*Sending email*')
-        logger.info('Email was successfully sended')
+        logger.info('*Sending email*')
+        logger.info(f'Email was successfully sended to user {user_id}')
         notification_log = NotificationLog(user_id = user_id, notification_topic = notification_topic, notification_message = notification_message)
         session.add(notification_log)
         await session.commit()
@@ -94,14 +95,14 @@ async def handle_settings_updated(data: dict, session: AsyncSession):
     notification_settings_is_exist = await session.execute(select(NotificationSettings).where(NotificationSettings.user_id == user_id))
     db_notification_settings = notification_settings_is_exist.scalar_one_or_none()
     if db_notification_settings is None:
-        logger.info('Settings not found')
+        logger.warning(f'Settings user {user_id} not found')
     else:
         if monthly_budget_exceeded_notification is not None:
             db_notification_settings.monthly_budget_exceeded_notification = monthly_budget_exceeded_notification
         if weekly_summary_notification is not None:
             db_notification_settings.weekly_summary_notification = weekly_summary_notification
         await session.commit()
-        logger.info('Settings successfully updated')
+        logger.info(f'Settings for user {user_id} successfully updated')
 
 if __name__ == "__main__":
     handlers = {
