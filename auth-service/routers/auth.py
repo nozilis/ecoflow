d3888785dde89cec2f2db330b827e37837834c61
nozilis <kreonix06110611@gmail.com>
2026-07-28 22:54:57 +0300

    refactor(auth-service): rework endpoints for single responsibility

--- auth-service/routers/auth.py
+++ auth-service/routers/auth.py
@@ -1,16 +1,11 @@
 from fastapi import APIRouter, HTTPException, status, Depends
-from sqlalchemy.exc import IntegrityError
 from schemas import UserCreate, UserResponse, UserLogin
-from dependencies import get_db, get_current_user
-from sqlalchemy.ext.asyncio import AsyncSession
-from passlib.hash import bcrypt
+from dependencies import get_auth_service, get_current_user
 from models import User
-from sqlalchemy import select, or_
-from jwt_token import create_access_token
-from publisher import publish_user_events
-import logging
+from services.auth_core import AuthService, UsernameAndEmailConflict, UsernameConflict, EmailConflict, InvalidUsernameOrPassword
+from typing import NewType
 
-logger = logging.getLogger(__name__)
+AccessToken = NewType('AccessToken', str)
 
 router = APIRouter(
     prefix='/auth',
@@ -18,44 +13,33 @@ router = APIRouter(
 )
 
 @router.post('/register', status_code=status.HTTP_201_CREATED)
-async def register_user(user: UserCreate, db: AsyncSession = Depends(get_db)):
-    hashed_password = bcrypt.hash(user.password)
-    user_is_exist = await db.execute(select(User).where(or_(User.username == user.username, User.email == user.email)))
-    db_user = user_is_exist.scalar_one_or_none()
-    if db_user is None:
-        create_user = User(username = user.username, hashed_password = hashed_password, email = user.email)
-        try:
-            db.add(create_user)
-            await db.commit()
-            await publish_user_events('created', create_user.id, username=user.username, email=user.email, created_at=create_user.created_at)
-            logger.info('User successfully registered')
-            return UserResponse.model_validate(create_user)
-        except IntegrityError as e:
-            pg_code = e.orig.diag.message_detail
-            await db.rollback()
-            logger.error(f'IntegrityError during registration: {pg_code}')
-            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f'Username or email is already taken')
-    elif db_user.username == user.username:
-        logger.warning('Username is already taken')
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f'Username is already taken')
-    else:
-        logger.warning('Email is already taken')
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f'Email is already taken')
+async def register_user(
+    user: UserCreate, 
+    auth_service: AuthService = Depends(get_auth_service)
+    ) -> UserResponse:
+    try:
+        created_user = await auth_service.register_user(user)
+        return UserResponse.model_validate(created_user)
+    except UsernameAndEmailConflict as exc:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f'{exc}')
+    except UsernameConflict as exc:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f'{exc}')
+    except EmailConflict as exc:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f'{exc}')
 
 @router.post('/login', status_code=status.HTTP_200_OK)
-async def login_user(user: UserLogin, db: AsyncSession = Depends(get_db)):
-    user_is_exist = await db.execute(select(User).where(User.username == user.username))
-    db_user = user_is_exist.scalar_one_or_none()
-    if db_user is None:
-        logger.warning('User not found')
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid username or password')
-    elif bcrypt.verify(user.password, db_user.hashed_password):
-        logger.info('User successfully login')
-        return create_access_token({'sub': str(db_user.id)})
-    else:
-        logger.warning('User fails verification')
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid username or password')
+async def login_user(
+    user: UserLogin, 
+    auth_service: AuthService = Depends(get_auth_service)
+) -> AccessToken:
+    try:
+        access_token = await auth_service.login_user(user)
+        return access_token
+    except InvalidUsernameOrPassword as exc:
+         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f'{exc}')
     
 @router.get('/whoami', status_code=status.HTTP_200_OK)
-async def check_user(user: User = Depends(get_current_user)):
+async def check_user(
+    user: User = Depends(get_current_user)
+) -> UserResponse:
     return UserResponse.model_validate(user)
