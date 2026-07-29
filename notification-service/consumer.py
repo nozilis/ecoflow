aea8935fd70ca59c4d3ef102b31349b9e5e9f750
nozilis <kreonix06110611@gmail.com>
2026-07-29 22:46:44 +0300

    refactor(notification-service): rework handlers for single responsibility

--- notification-service/consumer.py
+++ notification-service/consumer.py
@@ -3,12 +3,8 @@ import asyncio
 import json
 from database import async_session_maker
 from decouple import config
-from sqlalchemy import select
-from models import UserContact, NotificationSettings, NotificationLog
-import logging
 from sqlalchemy.ext.asyncio import AsyncSession
-
-logger = logging.getLogger(__name__)
+from services.notification_consumer_core import NotificationConsumerService
 
 async def run_consumer(handlers: dict[str, callable]):
     connection = await aio_pika.connect_robust(
@@ -34,75 +30,40 @@ async def run_consumer(handlers: dict[str, callable]):
                         async with async_session_maker() as session:
                             await handler(data, session)
 
-async def handle_user_created(data: dict, session: AsyncSession):
-    user_id, email, username = data['id'], data['username'], data['email']
-    user_contact_is_exist = await session.execute(select(UserContact).where(UserContact.username == username))
-    db_user_contact = user_contact_is_exist.scalar_one_or_none()
-    if db_user_contact is None:
-        create_user_contact = UserContact(user_id = user_id, email = email, username = username)
-        create_notification_settings = NotificationSettings(user_id = user_id)
-        session.add(create_user_contact)
-        session.add(create_notification_settings)
-        await session.commit()
-        logger.info(f"User {user_id} successfully created")
-    else:
-        logger.warning(f"User {user_id} already exists in UserContact, skipping")
+async def handle_user_created(
+    data: dict, 
+    session: AsyncSession
+):
+    notification_consumer_service = NotificationConsumerService(data, session)
+    await notification_consumer_service.handle_user_created()
 
-async def handle_user_updated(data: dict, session: AsyncSession):
-    user_id, username, email = data['user_id'], data.get('username'), data.get('email')
-    user_contact_is_exist = await session.execute(select(UserContact).where(UserContact.user_id == user_id))
-    db_user_contact = user_contact_is_exist.scalar_one_or_none()
-    if db_user_contact is None:
-        logger.warning(f'User {user_id} not found')
-    else:
-        if username:
-            db_user_contact.username = username
-        if email:
-            db_user_contact.email = email
-        await session.commit()
-        logger.info(f'User {user_id} successfully updated')
+async def handle_user_updated(
+    data: dict, 
+    session: AsyncSession
+):
+    notification_consumer_service = NotificationConsumerService(data, session)
+    await notification_consumer_service.handle_user_updated()
 
-async def handle_user_deleted(data: dict, session: AsyncSession):
-    user_id = data['user_id']
-    user_contact_is_exist = await session.execute(select(UserContact).where(UserContact.user_id == user_id))
-    db_user_contact = user_contact_is_exist.scalar_one_or_none()
-    if db_user_contact is None:
-            logger.warning(f'User {user_id} not found')
-    else:
-        await session.delete(db_user_contact)
-        await session.commit()
-        logger.info(f'User {user_id} successfully deleted')
+async def handle_user_deleted(
+    data: dict, 
+    session: AsyncSession
+):
+    notification_consumer_service = NotificationConsumerService(data, session)
+    await notification_consumer_service.handle_user_deleted()
 
-async def handle_budget_exceed(data: dict, session: AsyncSession):
-    user_id, monthly_stats_total, user_budget_limit = data['user_id'], data['monthly_stats_total'], data['user_budget_limit']
-    difference = monthly_stats_total - user_budget_limit
-    username = await session.execute(select(UserContact.username).where(UserContact.user_id == user_id))
-    db_username = username.scalar_one_or_none()
-    if db_username is None:
-        logger.warning(f'User {user_id} not found')
-    else:
-        notification_topic = 'Budget exceed'
-        notification_message = f'Hello, {db_username}, your spending has exceeded the expected limit by {difference}'
-        logger.info('*Sending email*')
-        logger.info(f'Email was successfully sended to user {user_id}')
-        notification_log = NotificationLog(user_id = user_id, notification_topic = notification_topic, notification_message = notification_message)
-        session.add(notification_log)
-        await session.commit()
-        logger.info('Notification log was successfully created')
+async def handle_budget_exceed(
+    data: dict, 
+    session: AsyncSession
+):
+    notification_consumer_service = NotificationConsumerService(data, session)
+    await notification_consumer_service.handle_budget_exceed()
 
-async def handle_settings_updated(data: dict, session: AsyncSession):
-    user_id, monthly_budget_exceeded_notification, weekly_summary_notification = data['user_id'], data.get('monthly_budget_exceeded_notification'), data.get('weekly_summary_notification')
-    notification_settings_is_exist = await session.execute(select(NotificationSettings).where(NotificationSettings.user_id == user_id))
-    db_notification_settings = notification_settings_is_exist.scalar_one_or_none()
-    if db_notification_settings is None:
-        logger.warning(f'Settings user {user_id} not found')
-    else:
-        if monthly_budget_exceeded_notification is not None:
-            db_notification_settings.monthly_budget_exceeded_notification = monthly_budget_exceeded_notification
-        if weekly_summary_notification is not None:
-            db_notification_settings.weekly_summary_notification = weekly_summary_notification
-        await session.commit()
-        logger.info(f'Settings for user {user_id} successfully updated')
+async def handle_settings_updated(
+    data: dict, 
+    session: AsyncSession
+):
+    notification_consumer_service = NotificationConsumerService(data, session)
+    await notification_consumer_service.handle_settings_updated()
 
 if __name__ == "__main__":
     handlers = {
