d48ca30836ee6fa5fb31b6eb816fad12a22ebda1
nozilis <kreonix06110611@gmail.com>
2026-07-29 22:20:06 +0300

    refactor(analytics-service): rework handlers for single responsibility

--- analytics-service/consumer.py
+++ analytics-service/consumer.py
@@ -3,12 +3,9 @@ import aio_pika
 from decouple import config
 import json
 from database import async_session_maker
-from datetime import datetime
-from sqlalchemy import select, func
-from models import MonthlyStats, UserBudget
 import logging
 from sqlalchemy.ext.asyncio import AsyncSession
-from publisher import publish_analytics_events
+from services.analytics_consumer_core import AnalyticsConsumerService
 
 logger = logging.getLogger(__name__)
 
@@ -36,97 +33,40 @@ async def run_consumer(handlers: dict[str, callable]):
                         async with async_session_maker() as session: 
                             await handler(data, session)
 
-async def handle_transaction_created(data: dict, session: AsyncSession):
-    date = datetime.fromisoformat(data['created_at'])
-    year, month = date.year, date.month
-    user_id, category, transaction_type, amount = data['user_id'], data['category'], data['transaction_type'], data['amount']
-    monthly_stats_is_exist = await session.execute(select(MonthlyStats).where(MonthlyStats.user_id == user_id, MonthlyStats.year == year, MonthlyStats.month == month, MonthlyStats.category == category))
-    db_monthly_stats = monthly_stats_is_exist.scalar_one_or_none()
-    if db_monthly_stats is None:
-        db_monthly_stats = MonthlyStats(user_id = user_id, year = year, month = month, category = category, transaction_type = transaction_type, total_amount = amount)
-        session.add(db_monthly_stats)
-        logger.info(f'MonthlyStats for user {user_id} successfully created')
-    else:
-        db_monthly_stats.total_amount += amount
-        logger.info(f'Total amount for user {user_id} successfully increased')
-    monthly_stats_total = await session.execute(select(func.sum(MonthlyStats.total_amount).label('monthly_stats_total')).where(MonthlyStats.user_id == user_id, MonthlyStats.year == year, MonthlyStats.month == month, MonthlyStats.transaction_type == 'Expense'))
-    user_budget_limit = await session.execute(select(UserBudget).where(UserBudget.user_id == user_id))
-    db_monthly_stats_total = monthly_stats_total.scalar_one_or_none()
-    db_user_budget_limit = user_budget_limit.scalar_one_or_none()
-    if db_monthly_stats_total is None:
-        logger.warning(f'User {user_id} transactions not found')
-    if db_user_budget_limit is None:
-        logger.warning(f'User {user_id} budget limit not found')
-    if db_monthly_stats_total and db_user_budget_limit:
-        if db_monthly_stats_total > db_user_budget_limit:
-            await publish_analytics_events('exceed', user_id, monthly_stats_total=db_monthly_stats_total, user_budget_limit=db_user_budget_limit)
-            logger.info(f'User {user_id} budget exceed the limit event successfully published')
-    await session.commit()
+async def handle_transaction_created(
+    data: dict,
+    session: AsyncSession,
+):
+    analytics_consumer_service = AnalyticsConsumerService(data, session)
+    await analytics_consumer_service.handle_transaction_created()
 
-async def handle_transaction_updated(data: dict, session: AsyncSession):
-    date = datetime.fromisoformat(data['created_at'])
-    year, month = date.year, date.month
-    user_id, category, transaction_type, amount, recent_type, recent_amount = data['user_id'], data['category'], data['transaction_type'], data['amount'], data['recent_type'], data['recent_amount']
-    monthly_stats_is_exist = await session.execute(select(MonthlyStats).where(MonthlyStats.user_id == user_id, MonthlyStats.year == year, MonthlyStats.month == month, MonthlyStats.category == category))
-    db_monthly_stats = monthly_stats_is_exist.scalar_one_or_none()
-    if db_monthly_stats:
-        if recent_type == 'Income':
-            old_impact = recent_amount
-        else:
-            old_impact = -recent_amount
-        if transaction_type == 'Income':
-            new_impact = amount
-        else:
-            new_impact = -amount
-        db_monthly_stats.total_amount = db_monthly_stats.total_amount - old_impact + new_impact
-        await session.commit()
-        logger.info(f'MonthlyStats for user {user_id} successfully updated')
-    else:
-        logger.warning(f'MonthlyStats for user {user_id} not found')
+async def handle_transaction_updated(
+    data: dict, 
+    session: AsyncSession
+):
+    analytics_consumer_service = AnalyticsConsumerService(data, session)
+    await analytics_consumer_service.handle_transaction_updated()
 
-async def handle_transaction_deleted(data: dict, session: AsyncSession):
-    date = datetime.fromisoformat(data['created_at'])
-    year, month = date.year, date.month
-    user_id, category, transaction_type, amount = data['user_id'], data['category'], data['transaction_type'], data['amount']
-    monthly_stats_is_exist = await session.execute(select(MonthlyStats).where(MonthlyStats.user_id == user_id, MonthlyStats.year == year, MonthlyStats.month == month, MonthlyStats.category == category))
-    db_monthly_stats = monthly_stats_is_exist.scalar_one_or_none()
-    if db_monthly_stats:
-        if transaction_type == 'Income':
-            db_monthly_stats.total_amount -= amount
-        else:
-            db_monthly_stats.total_amount += amount
-        await session.commit()
-        logger.info(f'MonthlyStats for user {user_id} successfully updated')
-    else:
-        logger.warning(f'MonthlyStats for user {user_id} not found')
+async def handle_transaction_deleted(
+    data: dict, 
+    session: AsyncSession
+):
+    analytics_consumer_service = AnalyticsConsumerService(data, session)
+    await analytics_consumer_service.handle_transaction_deleted()
 
-async def handle_budget_limit_updated(data: dict, session: AsyncSession):
-    user_id, budget_limit = data['user_id'], data['budget_limit']
-    user_budget_is_exist = await session.execute(select(UserBudget).where(UserBudget.user_id == user_id))
-    db_user_budget = user_budget_is_exist.scalar_one_or_none()
-    if db_user_budget is None:
-        db_user_budget = UserBudget(user_id=user_id, budget_limit=budget_limit)
-        session.add(db_user_budget)
-        logger.info(f'Budget limit for user {user_id} successfully added')
-    else:
-        db_user_budget.budget_limit = budget_limit
-        logger.info(f'Budget limit for user {user_id} successfully updated')
-    await session.commit()
+async def handle_budget_limit_updated(
+    data: dict, 
+    session: AsyncSession
+):
+    analytics_consumer_service = AnalyticsConsumerService(data, session)
+    await analytics_consumer_service.handle_budget_limit_updated()
 
-async def handle_user_deleted(data: dict, session: AsyncSession):
-    user_id = data['user_id']
-    user_monthly_stats_is_exist = await session.execute(select(MonthlyStats).where(MonthlyStats.user_id == user_id))
-    user_budget_is_exist = await session.execute(select(UserBudget).where(UserBudget.user_id == user_id))
-    db_user_monthly_stats = user_monthly_stats_is_exist.scalars().all()
-    db_user_budget = user_budget_is_exist.scalar_one_or_none()
-    if db_user_monthly_stats:
-        for monthly_stats in db_user_monthly_stats:
-            await session.delete(monthly_stats)
-        logger.info(f'MonthlyStats for user {user_id} successfully deleted')
-    if db_user_budget is not None:
-        await session.delete(db_user_budget)
-        logger.info(f'UserBudget for user {user_id} successfully deleted')
-    await session.commit()
+async def handle_user_deleted(
+    data: dict, 
+    session: AsyncSession
+):
+    analytics_consumer_service = AnalyticsConsumerService(data, session)
+    await analytics_consumer_service.handle_user_deleted()
 
 if __name__ == "__main__":
     handlers = {
