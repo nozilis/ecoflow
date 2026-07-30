c7b9ca466f2b0b35597bd0b77c6008f7002d8449
nozilis <kreonix06110611@gmail.com>
2026-07-30 04:11:50 +0300

    feat(transaction-service): add TransactionConsumerService class for consumer business logic

---
+++ transaction-service/services/transaction_consumer_core.py
@@ -0,0 +1,20 @@
+from models import Transaction
+from sqlalchemy import select
+import logging
+
+logger = logging.getLogger(__name__)
+
+class TransactionConsumerService:
+    def __init__(self, data, session):
+        self.data = data
+        self.session = session
+
+    async def handle_user_deleted(self):
+        user_id = self.data['user_id']
+        user_transactions_is_exist = await self.session.execute(select(Transaction).where(Transaction.user_id == user_id))
+        db_user_transactions = user_transactions_is_exist.scalars().all()
+        if db_user_transactions:
+            for transaction in db_user_transactions:
+                await self.session.delete(transaction)
+            await self.session.commit()
+            logger.info(f'Transactions for user {user_id} successfully deleted')
