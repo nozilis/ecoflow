29d336b3f9cbd9902786cf9bfae70e114c9c9f7e
nozilis <kreonix06110611@gmail.com>
2026-08-02 22:30:10 +0300

    feat(transaction-service): add redis pool to app lifespan

--- transaction-service/main.py
+++ transaction-service/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from routers import transaction
 from database import engine
 from contextlib import asynccontextmanager
+from redis_client import redis_pool
 import logging
 
 logger = logging.getLogger(__name__)
@@ -9,8 +10,10 @@ logger = logging.getLogger(__name__)
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     logger.info('App starting up')
+    app.state.redis_pool = redis_pool
     yield
     logger.info('App shuting down')
+    await redis_pool.aclose()
     await engine.dispose()
 
 app = FastAPI(lifespan=lifespan, title='Transaction Service API')
