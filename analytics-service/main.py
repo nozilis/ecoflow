1af7281a7e78c504b3afcc767bba61a1e89e2bdf
nozilis <kreonix06110611@gmail.com>
2026-08-02 17:34:24 +0300

    feat(analytics-service): add redis pool to app lifespan

--- analytics-service/main.py
+++ analytics-service/main.py
@@ -3,14 +3,17 @@ from routers import analytics
 from database import engine
 from contextlib import asynccontextmanager
 import logging
+from redis_client import redis_pool
 
 logger = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     logger.info('App starting up')
+    app.state.redis_pool = redis_pool
     yield
     logger.info('App shuting down')
+    await redis_pool.aclose()
     await engine.dispose()
 
 app = FastAPI(lifespan=lifespan, title='Analytics Service API')
