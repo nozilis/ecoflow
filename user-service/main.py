515a55aaf92fa5765d6fb7e239408ca5ac1ca2ac
nozilis <kreonix06110611@gmail.com>
2026-08-03 03:27:39 +0300

    feat(user-service): add redis pool to app lifespan

--- user-service/main.py
+++ user-service/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from database import engine
 from routers import user_profile
+from redis_client import redis_pool
 import logging
 
 logger = logging.getLogger(__name__)
@@ -9,8 +10,10 @@ logger = logging.getLogger(__name__)
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     logger.info('App starting up')
+    app.state.redis_pool = redis_pool
     yield
     logger.info('App shuting down')
+    await redis_pool.aclose()
     await engine.dispose()
 
 app = FastAPI(lifespan=lifespan, title='User Service API')
