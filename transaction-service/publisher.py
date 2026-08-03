7fda7eb16b765c2641a8c180e81bf961f3b42691
nozilis <kreonix06110611@gmail.com>
2026-08-04 00:15:40 +0300

    refactor(analytics-service, auth-service, transaction-service, user-service): rework publish_events func and publish_*_events func

--- transaction-service/publisher.py
+++ transaction-service/publisher.py
@@ -1,5 +1,4 @@
 import aio_pika
-from decouple import config
 import json
 from datetime import datetime
 from enum import Enum
@@ -7,30 +6,26 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-async def publish_event(message_body, routing_key):
-    connection = await aio_pika.connect_robust(
-        f"amqp://{config('RABBITMQ_DEFAULT_USER')}:{config('RABBITMQ_DEFAULT_PASS')}@rabbitmq/"
+async def publish_events(message_body, routing_key, connection):
+    channel = await connection.channel()
+
+    message = aio_pika.Message(
+        body=message_body,
+        delivery_mode=aio_pika.DeliveryMode.PERSISTENT
     )
 
-    async with connection:
-        channel = await connection.channel()
+    exchange = await channel.declare_exchange("ecoflow_events", aio_pika.ExchangeType.TOPIC, durable=True)
 
-        message = aio_pika.Message(
-            body=message_body,
-            delivery_mode=aio_pika.DeliveryMode.PERSISTENT
-        )
+    await exchange.publish(
+        message=message,
+        routing_key=routing_key
+    )
 
-        exchange = await channel.declare_exchange("ecoflow_events", aio_pika.ExchangeType.TOPIC, durable=True)
+    logger.info(f'Sent: {message_body}')
+    await channel.close()
 
-        await exchange.publish(
-            message=message,
-            routing_key=routing_key
-        )
-
-        logger.info(f'Sent: {message_body}')
-
-async def publish_transaction_events(event_type, user_id, **kwargs):
+async def publish_transaction_events(event_type, user_id, connection, **kwargs):
     data_dict = {'user_id': user_id, **kwargs} 
     message_body = json.dumps(data_dict, default=lambda o:o.value if isinstance(o, Enum) else (o.isoformat() if isinstance(o, datetime) else None)).encode("utf-8")
-    await publish_event(message_body, f'transaction.{event_type}')
+    await publish_events(message_body, f'transaction.{event_type}', connection)
     logger.info(f'Event {event_type} by user {user_id} successfully published')
--- user-service/publisher.py
+++ user-service/publisher.py
@@ -1,34 +1,29 @@
 import aio_pika
-from decouple import config
 import json
 import logging
 
 logger = logging.getLogger(__name__)
 
-async def publish_event(message_body, routing_key):
-    connection = await aio_pika.connect_robust(
-        f"amqp://{config('RABBITMQ_DEFAULT_USER')}:{config('RABBITMQ_DEFAULT_PASS')}@rabbitmq/"
+async def publish_events(message_body, routing_key, connection):
+    channel = await connection.channel()
+
+    message = aio_pika.Message(
+        body=message_body,
+        delivery_mode=aio_pika.DeliveryMode.PERSISTENT
     )
 
-    async with connection:
-        channel = await connection.channel()
+    exchange = await channel.declare_exchange("ecoflow_events", aio_pika.ExchangeType.TOPIC, durable=True)
 
-        message = aio_pika.Message(
-            body=message_body,
-            delivery_mode=aio_pika.DeliveryMode.PERSISTENT
-        )
+    await exchange.publish(
+        message=message,
+        routing_key=routing_key
+    )
 
-        exchange = await channel.declare_exchange("ecoflow_events", aio_pika.ExchangeType.TOPIC, durable=True)
+    logger.info(f'Sent: {message_body}')
+    await channel.close()
 
-        await exchange.publish(
-            message=message,
-            routing_key=routing_key
-        )
-
-        logger.info(f'Sent: {message_body}')
-
-async def publish_user_events(event_type, user_id, **kwargs):
+async def publish_user_events(event_type, user_id, connection, **kwargs):
     data_dict = {'user_id': user_id, **kwargs}
     message_body = json.dumps(data_dict).encode("utf-8")
-    await publish_event(message_body, f'user.{event_type}')
+    await publish_events(message_body, f'user.{event_type}', connection)
     logger.info(f'Event {event_type} by user {user_id} successfully published')
