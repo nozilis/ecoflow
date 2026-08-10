3f882733b6ceb93e919475083e49d2c90e0b8f81
nozilis <kreonix06110611@gmail.com>
2026-08-10 16:30:55 +0300

    fix(transaction-service): use None as default category to fix validation of unset field

--- transaction-service/schemas.py
+++ transaction-service/schemas.py
@@ -6,17 +6,23 @@ from datetime import datetime
 class TransactionCreate(BaseModel):
     amount: int
     transaction_type: TransactionType
-    category: Optional[ExpenseCategory | IncomeCategory] = 'Other'  #Union enum classes for typification
+    category: Optional[ExpenseCategory | IncomeCategory] = None #Union enum classes for typification
     description: Optional[str] = None
 
     @model_validator(mode='after')
     def check_category_type(self) -> Self:
-        if self.transaction_type == TransactionType.EXPENSE:
-            if self.category not in ExpenseCategory:
-                raise ValueError('Категория транзакции заполнена неверно!')
+        if self.category:
+            if self.transaction_type == TransactionType.EXPENSE:
+                if self.category not in ExpenseCategory:
+                    raise ValueError('Invalid transaction category')
+            else:
+                if self.category not in IncomeCategory:
+                    raise ValueError('Invalid transaction category')
         else:
-            if self.category not in IncomeCategory:
-                raise ValueError('Категория транзакции заполнена неверно!')
+            if self.transaction_type == TransactionType.EXPENSE:
+                self.category = ExpenseCategory.OTHER
+            else:
+                self.category = IncomeCategory.OTHER
         return self
     
 class TransactionResponse(BaseModel):
